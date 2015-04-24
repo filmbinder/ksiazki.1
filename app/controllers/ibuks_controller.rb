8f4ac259fdb6a66778dedd08f6b1d3ffcfc00dc4
Prelang Builder <builder@prelang.com>
2015-04-24 06:17:52 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/ibuks_controller.rb
+++ app/controllers/ibuks_controller.rb
@@ -1,4 +1,8 @@
 class IbuksController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_ibuk, only: [:show, :edit, :update, :destroy]
 
   # GET /ibuks
